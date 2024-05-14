793b989488448b6755982a97d8566fe291390225
mullet <mullet@gmail.com>
2024-05-14 18:21:38 -0300

    added results for searchbar input

--- src/app/maps/components/search-box/search-box.component.ts
+++ src/app/maps/components/search-box/search-box.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { PlacesService } from '../../services/places.service';
 
 @Component({
   selector: 'app-search-box',
   templateUrl: './search-box.component.html',
-  styleUrl: './search-box.component.css'
+  styleUrl: './search-box.component.css',
 })
 export class SearchBoxComponent {
+  private debounceTimer?: NodeJS.Timeout;
 
+  constructor(private placesService: PlacesService) {}
+
+  public onQueryChanged(query: string = ''): void {
+    if (this.debounceTimer) clearTimeout(this.debounceTimer);
+
+    this.debounceTimer = setTimeout(() => {
+      this.placesService.getPlacesByQuery(query);
+    }, 350);
+  }
 }
--- src/app/maps/services/places.service.ts
+++ src/app/maps/services/places.service.ts
@@ -1,17 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Feature, PlacesResponse } from '../interfaces/places-response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlacesService {
 
-  public userLocation: [number, number] | undefined;
+  public userLocation:    [number, number] | undefined;
+  public isLoadingPlaces: boolean = false;
+  public places:          Feature[] = []
 
   get isUserLocationReady() {
     return !!this.userLocation;
   }
 
-  constructor() {
+  constructor(
+    private http: HttpClient,
+  ) {
     this.getUserLocation();
   }
 
@@ -31,4 +37,16 @@ export class PlacesService {
       }
     })
   };
+
+  getPlacesByQuery( query: string ) {
+
+    this.isLoadingPlaces = true;
+
+    return this.http.get<PlacesResponse>(`https://api.mapbox.com/search/geocode/v6/forward?q=${query}&proximity=-64.23738585247824%2C-31.34096637708827&language=es&access_token=pk.eyJ1IjoibXVsbGV0bWFudSIsImEiOiJjbHVzaWp3Z3EwajE5Mmpua3Y1Nm00NWIzIn0.BXH02qD27MpXTxHmKUcGfQ`)
+     .subscribe( (res) => {
+        this.isLoadingPlaces = false;
+        this.places = res.features;
+     })
+  }
+
 }
